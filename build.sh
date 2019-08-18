ed7d0198c60b2b0da01988e171b9aa8db8457b00
tindy2013 <49061470+tindy2013@users.noreply.github.com>
2019-08-19 02:27:01 +0800

    Update build scripts Remove unused files

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -e
 mkdir obj
 mkdir bin
+gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c geoip.cpp -o obj/geoip.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c logger.cpp -o obj/logger.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c main.cpp -o obj/main.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c misc.cpp -o obj/misc.o
@@ -9,10 +10,11 @@ gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c multithread-test.cpp
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c printout.cpp -o obj/printout.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c processes.cpp -o obj/processes.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c renderer.cpp -o obj/renderer.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c rulematch.cpp -o obj/rulematch.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c socket.cpp -o obj/socket.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c speedtestutil.cpp -o obj/speedtestutil.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c tcping.cpp -o obj/tcping.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c webget.cpp -o obj/webget.o
-g++ -g -o stairspeedtest obj/logger.o obj/main.o obj/misc.o obj/multithread-test.o obj/printout.o obj/processes.o obj/renderer.o obj/socket.o obj/speedtestutil.o obj/tcping.o obj/webget.o -lcurl -lPNGwriter -lpng16 -lfreetype -lz -lssl -lcrypto -ldl -lpthread -lyaml-cpp -s
+g++ -g -o stairspeedtest obj/geoip.o obj/logger.o obj/main.o obj/misc.o obj/multithread-test.o obj/printout.o obj/processes.o obj/renderer.o obj/rulematch.o obj/socket.o obj/speedtestutil.o obj/tcping.o obj/webget.o -lcurl -lPNGwriter -lpng16 -lfreetype -lz -lssl -lcrypto -ldl -lpthread -lyaml-cpp -s
 chmod +x stairspeedtest
 
