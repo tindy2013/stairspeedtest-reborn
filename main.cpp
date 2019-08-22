adda67170465f63000e25acf529e448658cef273
Tindy X <49061470+tindy2013@users.noreply.github.com>
2019-08-22 23:47:58 +0800

    Fix bugs in previous pull request

--- main.cpp
+++ main.cpp
@@ -725,10 +725,9 @@ int main(int argc, char* argv[])
                 if(rpcmode)
                 {
                     clearTrans();
-                    addTrans("?id?", node.id);
+                    addTrans("?id?", to_string(node.id));
                     addTrans("?group?", node.group);
                     addTrans("?remarks?", node.remarks);
-                    addTrans("?index?", to_string(node.id + 1));
                     printMsgWithDict(SPEEDTEST_MESSAGE_GOTSERVER, rpcmode, dict, trans);
                 }
                 singleTest(&node);
