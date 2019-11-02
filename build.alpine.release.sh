e2deb8e523fa619766f4f2908dab3f6cbdcacb22
Tindy X <49061470+tindy2013@users.noreply.github.com>
2019-11-02 10:39:14 +0800

    Add Travis-CI build script

--- build.alpine.release.sh
+++ build.alpine.release.sh
@@ -4,10 +4,10 @@ mkdir bin
 mkdir tools/clients
 rm -rf tools/clients/*
 rm tools/gui/websocketd.exe
-set -e
+set -xe
 
 apk add gcc g++ cmake make autoconf automake libtool build-base linux-headers
-apk add libpng-dev libressl-dev freetype-dev freetype-static zlib-dev rapidjson-dev mbedtls-dev mbedtls-static libev-dev pcre-dev libsodium-dev c-ares-dev
+apk add libpng-dev libpng-static libressl-dev freetype-dev freetype-static zlib-dev rapidjson-dev mbedtls-dev mbedtls-static libev-dev pcre-dev libsodium-dev libsodium-static c-ares-dev libevent-dev libevent-static
 
 git clone https://github.com/curl/curl
 cd curl
@@ -80,22 +80,24 @@ unzip v2ray-linux-64.zip v2ray v2ctl
 mv v2ray tools/clients/
 mv v2ctl tools/clients/
 
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c geoip.cpp -o obj/geoip.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c logger.cpp -o obj/logger.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c main.cpp -o obj/main.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c misc.cpp -o obj/misc.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c multithread_test.cpp -o obj/multithread_test.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c printout.cpp -o obj/printout.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c processes.cpp -o obj/processes.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c rapidjson_extra.cpp -o obj/rapidjson_extra.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -I/usr/local/include/freetype2 -I/usr/include/freetype2 -std=c++11   -c renderer.cpp -o obj/renderer.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c rulematch.cpp -o obj/rulematch.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c socket.cpp -o obj/socket.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c speedtestutil.cpp -o obj/speedtestutil.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c tcping.cpp -o obj/tcping.o
-gcc -Wall -fexceptions  -DCURL_STATICLIB -g -std=c++11   -c webget.cpp -o obj/webget.o
-g++ -g -o stairspeedtest obj/geoip.o obj/logger.o obj/main.o obj/misc.o obj/multithread_test.o obj/printout.o obj/processes.o obj/rapidjson_extra.o obj/renderer.o obj/rulematch.o obj/socket.o obj/speedtestutil.o obj/tcping.o obj/webget.o -lcurl -lPNGwriter -lpng16 -lfreetype -lz -lssl -lcrypto -ldl -lpthread -lyaml-cpp -lbz2 -s -static
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c geoip.cpp -o obj/geoip.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c logger.cpp -o obj/logger.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c main.cpp -o obj/main.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c misc.cpp -o obj/misc.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c multithread_test.cpp -o obj/multithread_test.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c printout.cpp -o obj/printout.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c processes.cpp -o obj/processes.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c rapidjson_extra.cpp -o obj/rapidjson_extra.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -I/usr/local/include/freetype2 -I/usr/include/freetype2 -std=c++17   -c renderer.cpp -o obj/renderer.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c rulematch.cpp -o obj/rulematch.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c socket.cpp -o obj/socket.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c speedtestutil.cpp -o obj/speedtestutil.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c tcping.cpp -o obj/tcping.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c webget.cpp -o obj/webget.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c webgui_wrapper.cpp -o obj/webgui_wrapper.o
+gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c webserver_libevent.cpp -o obj/webserver_libevent.o
+g++ -o stairspeedtest obj/geoip.o obj/logger.o obj/main.o obj/misc.o obj/multithread_test.o obj/printout.o obj/processes.o obj/rapidjson_extra.o obj/renderer.o obj/rulematch.o obj/socket.o obj/speedtestutil.o obj/tcping.o obj/webget.o obj/webgui_wrapper.o obj/webserver_libevent.o -levent -lcurl -lPNGwriter -lpng16 -lfreetype -lz -lssl -lcrypto -ldl -lpthread -lyaml-cpp -lbz2 -s -static
 
-chmod +rx stairspeedtest pref.ini webgui.sh tools/clients/* tools/gui/* tools/misc/*
-tar czf stairspeedtest_reborn_linux64.tar.gz stairspeedtest pref.ini webgui.sh tools/
+chmod +rx stairspeedtest pref.ini webgui.sh webserver.sh tools/clients/* tools/gui/* tools/misc/*
+tar czf stairspeedtest_reborn_linux64.tar.gz stairspeedtest pref.ini webgui.sh webserver.sh tools/
 
