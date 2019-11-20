8d1ed4ef27c77445b568a7fc80b360c92f364db5
Tindy X <49061470+tindy2013@users.noreply.github.com>
2019-11-20 21:51:26 +0800

    Update build scripts

--- build.alpine.release.sh
+++ build.alpine.release.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 mkdir obj
-mkdir bin
-mkdir tools/clients
-rm -rf tools/clients/*
-rm tools/gui/websocketd.exe
 set -xe
 
-apk add gcc g++ cmake make autoconf automake libtool build-base linux-headers
-apk add libpng-dev libpng-static libressl-dev freetype-dev freetype-static zlib-dev rapidjson-dev mbedtls-dev mbedtls-static libev-dev pcre-dev libsodium-dev libsodium-static c-ares-dev libevent-dev libevent-static
+apk add gcc g++ build-base linux-headers cmake make autoconf automake libtool
+apk add libpng-dev libpng-static libressl-dev freetype-dev freetype-static zlib-dev rapidjson-dev mbedtls-dev mbedtls-static libevent-dev libevent-static
+
+chmod +x build.alpine.clients.sh
+./build.alpine.clients.sh
 
 git clone https://github.com/curl/curl
 cd curl
 ./buildconf
-./configure
+./configure > /dev/null
 make install -j4
 cd ..
 
@@ -28,57 +27,10 @@ cmake .
 make install -j4
 cd ..
 
-git clone https://github.com/shadowsocks/simple-obfs
-cd simple-obfs
-git submodule init
-git submodule update
-./autogen.sh
-./configure --disable-documentation
-make -j4
-cd src
-gcc obfs_local*.o ../libcork/.libs/libcork.a -o simple-obfs -lev -s -static
-mv simple-obfs ../../tools/clients/
-cd ../..
-
-git clone https://github.com/shadowsocks/shadowsocks-libev
-cd shadowsocks-libev
-git submodule init
-git submodule update
-./autogen.sh
-./configure --disable-documentation
-make -j4
-cd src
-gcc ss_local*.o .libs/libshadowsocks-libev.a -o ss-local -lpcre -lmbedtls -lmbedcrypto -lev -lsodium -s -static
-mv ss-local ../../tools/clients/
-cd ../..
-
-git clone https://github.com/shadowsocksrr/shadowsocksr-libev
-cd shadowsocksr-libev
-git checkout -b latest origin/Akkariiin/develop
-./autogen.sh
-./configure --disable-documentation
-make -j4
-cd src
-gcc ss_local*.o .libs/libshadowsocks-libev.a ../libudns/.libs/libudns.a -o ssr-local -lpcre -lssl -lcrypto -lev -lsodium -s -static
-mv ssr-local ../../tools/clients/
-cd ../..
-
 git clone git://sourceware.org/git/bzip2.git
 cd bzip2
 make install -j4
 cd ..
-
-apk del wget
-apk add wget
-
-curl -LO https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip
-unzip websocketd-0.3.0-linux_amd64.zip websocketd
-mv websocketd tools/gui/
-
-curl -LO https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip v2ray v2ctl
-mv v2ray tools/clients/
-mv v2ctl tools/clients/
 
 gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c geoip.cpp -o obj/geoip.o
 gcc -Wall -fexceptions  -DCURL_STATICLIB -std=c++17   -c logger.cpp -o obj/logger.o
@@ -100,4 +52,3 @@ g++ -o stairspeedtest obj/geoip.o obj/logger.o obj/main.o obj/misc.o obj/multith
 
 chmod +rx stairspeedtest pref.ini webgui.sh webserver.sh tools/clients/* tools/gui/* tools/misc/*
 tar czf stairspeedtest_reborn_linux64.tar.gz stairspeedtest pref.ini webgui.sh webserver.sh tools/
-
