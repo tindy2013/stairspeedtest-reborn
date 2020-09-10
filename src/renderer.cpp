76e33aa48f6cac53bc84bc047ccb61c1c9e1f6b4
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-09-10 13:27:22 +0800

    Fix incorrect data range
    Update Travis CI build scripts.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -36,7 +36,7 @@ int calcLength(const std::string &data)
     int total = 0;
     for(unsigned int i = 0; i < data.size(); i++)
     {
-        if(int(data[i]) > 127)
+        if(int(data[i]) < 0)
             total += 2;
         else
             total++;
