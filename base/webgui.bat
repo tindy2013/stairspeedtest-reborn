790bc30f42079d6c5dbfe467b3ffcf19b79fb0d3
We1eVen <38682928+We1eVen@users.noreply.github.com>
2020-11-04 13:02:51 +0800

    Update webgui.bat

--- base/webgui.bat
+++ base/webgui.bat
@@ -2,5 +2,5 @@
 chcp 65001>nul
 title Stair Speedtest Web GUI Backend
 setlocal enabledelayedexpansion
-start http://127.0.0.1:65430/gui.html
+start http://127.0.0.1:65430
 tools\gui\websocketd --port=65430 --maxforks=1 --staticdir=tools\gui stairspeedtest /rpc
