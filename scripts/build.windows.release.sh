7224fd5f14d0cd90b9b0cf7463c587f9e2b2ef8a
Tindy X <49061470+tindy2013@users.noreply.github.com>
2022-01-05 12:02:06 +0800

    Fix Windows build script

--- scripts/build.windows.release.sh
+++ scripts/build.windows.release.sh
@@ -6,7 +6,7 @@ make -j4
 rm stairspeedtest.exe
 
 # this may change in the future
-export FREETYPE_DEPS=$(pkg-config --libs --static harfbuzz | sed -E "s/(-lbrotli\S+)/\1-static/g")
+export FREETYPE_DEPS=$(pkg-config --libs --static harfbuzz)
 # build resources
 windres -J rc -O coff -i res/res.rc -o res.res
 g++ $(find CMakeFiles/stairspeedtest.dir/src -name "*.obj") "$USERPROFILE/maindeps/curl/lib/libcurl.a" res.res -o base/stairspeedtest.exe -static -levent -lPNGwriter -lfreetype $FREETYPE_DEPS -lpng -lpcre2-8 -lyaml-cpp -lssl -lcrypto -lws2_32 -lwsock32 -lcrypt32 -liphlpapi  -lz -lbz2 -s
